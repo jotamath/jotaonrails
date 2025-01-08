f07798874848e12fb03151ad06489b717d30fa97
jotamath <01jmnc@gmail.com>
2025-01-08 16:46:49 -0300

    bufix render-build

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,6 +1,8 @@
-#!/bin/
-set -o errexit
+#!/bin/bash
+# Este é o shebang correto para garantir que o script use o Bash
+export PATH="/usr/local/bin:/usr/bin:$PATH"
 
+# Rodar o build
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
